4e7f56a5e2f65c68a4cbdd866291ca24aac3d47c
rudra pratap singh <prataprudra10102001@gmail.com>
2025-01-26 04:59:46 +0530

    Basic 0f ejs

--- EJS/Band-Generator-Project/index.js
+++ EJS/Band-Generator-Project/index.js
@@ -2,26 +2,37 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 //Step 3 - Make the styling show up.
+app.use(express.static("public"));
 //Hint 1: CSS files are static files!
 //Hint 2: The header and footer are partials.
 //Hint 3: Add the CSS link in header.ejs
 
 //Step 4 - Add a dynamic year to the footer.
+// const curr_year=new Date()
+// const Full_year=curr_year.getFullYear()
+// console.log(Full_year)
 //Hint: Google to find out how to get the current year using JS.
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   //Step 1 - Make the get route work and render the index.ejs file.
+  res.render("index.ejs")
 });
 
 app.post("/submit", (req, res) => {
   //Step 2 - Make the generate name functionality work
+  const random_adj=adj[Math.floor(Math.random()*adj.length)];
+  const random_noun=noun[Math.floor(Math.random()*noun.length)];
   //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
   //Then:
+  const gen_name=random_adj+" "+random_noun;
+  res.render("index.ejs",
+    {generate_name:gen_name}
+  )
   //1. You should randomly pick an adjective from the const "adj" and a noun from const "noun",
   //scroll down to see the two arrays.
   //2. Send the index.ejs as a response and add the adjective and noun to the res.render
