ca8c0f7c7050c16272e841c36e9d3259a4b8209d
rudra pratap singh <prataprudra10102001@gmail.com>
2025-01-26 02:36:50 +0530

    EJS tags

--- EJS/index.js
+++ EJS/index.js
@@ -4,26 +4,37 @@ import bodyParser from "body-parser";
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 
+// app.get("/",(req,res)=>
+// {
+//     const today=new Date();
+//     const day=today.getDay();
+
+//     // console.log(day)
+//     let type="a Week Day";
+//     let adv=" Work Harder"
+//     if (day===0||day===6)
+//     {
+//      type="The Weekend Day";
+//      adv="Chill "
+//     }
+//     res.render("index.ejs",{
+//         dayType:type,
+//         advice:adv,
+//     });
+// })
+// let bowls=["apples","Banana","Grapes"];
 app.get("/",(req,res)=>
 {
-    const today=new Date();
-    const day=today.getDay();
-
-    // console.log(day)
-    let type="a Week Day";
-    let adv=" Work Harder"
-    if (day===0||day===6)
-    {
-     type="The Weekend Day";
-     adv="Chill "
-    }
-    res.render("index.ejs",{
-        dayType:type,
-        advice:adv,
-    });
+    const data={
+        title:"Ejx Tags",
+        seconds:new Date().getSeconds(),
+        bowls:["apples","Banana","Grapes"],
+        html_content:"<em> This is some text</em>"
+    };
+    res.render("index.ejs",
+        {data}
+    );
 })
-
-
 
 
 app.listen(5000,()=>
